f895a2f478210707354a085816b095d3d4212812
za-sec <eremeevvanya1@gmail.com>
2020-12-04 12:09:35 +0300

    Create scanner.sh

---
+++ scanner.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+filein=$1
+
+fileout=$2
+
+
+if [[ ("$filein" == "-h") || ("$filein" == "")]]; then
+
+    echo "Type domains filename first, and space separated output filename second";
+
+	  exit 1;
+
+fi
+
+
+while IFS= read line
+	
+    do
+        	
+	      domain=$line
+                       
+        assetfinder -subs-only $domain | waybackurls | grep 'https' | grep 'url=.*https%3A' | grep -v 'signin\|login\|click\|embed' > $2
+                         
+    done <"$filein"
