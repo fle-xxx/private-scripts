94680d882a3d22cb0a002289a2091f3680f017dc
za-sec <eremeevvanya1@gmail.com>
2020-12-23 07:39:11 +0300

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -21,7 +21,7 @@ while IFS= read -r line
 	
 	do
         	
-        	assetfinder -subs-only "$line" | waybackurls | grep 'https' | grep 'url=\|https%3A\|aHR0cHM6Ly' | grep -v 'signin\|login\|click\|embed\|authorize\|web.archive.org\|counter';
+        	assetfinder -subs-only "$line" | waybackurls | grep 'https' | grep 'url=\|https%3A\|aHR0cHM6Ly' | grep -v 'signin\|login\|click\|embed\|authorize\|web.archive.org\|counter\|utm_referrer';
 
                                  
         done < "$filein" > "$tmpfile"
