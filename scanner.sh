b12e683f8edb906277c920993f2d284ac47a2cf9
za-sec <eremeevvanya1@gmail.com>
2020-12-25 21:07:05 +0300

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -21,7 +21,7 @@ while IFS= read -r line
 	
 	do
         	
-        	assetfinder -subs-only "$line" | waybackurls | grep 'https' | grep 'return_to\|redirect_uri\|next=\|=/\|host=\|path=\|src=\|imgurl\|file=\|u=\|link=\|url=\|https%3A\|aHR0cHM6Ly' | grep -v 'utm_source\|utm_content\|metric\|signin\|signup\|login\|authorize\|click\|embed\|web.archive.org\|counter\|analytics\|utm_referrer';
+        	assetfinder -subs-only "$line" | waybackurls | grep 'https' | grep '=%2F\|%3D%2F\|return_to\|redirect_uri\|next=\|=/\|host=\|path=\|src=\|imgurl\|file=\|u=\|link=\|url=\|https%3A\|aHR0cHM6Ly' | grep -v 'utm_source\|utm_content\|metric\|signin\|signup\|login\|authorize\|click\|embed\|web.archive.org\|counter\|analytics\|utm_referrer';
 
                                  
         done < "$filein" > "$tmpfile"
