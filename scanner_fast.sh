033b5a1836645ffd987c19c39eebd462935d9ea2
za-sec <eremeevvanya1@gmail.com>
2021-02-13 20:57:10 +0300

    Update scanner_fast.sh

--- scanner_fast.sh
+++ scanner_fast.sh
@@ -1,66 +1,34 @@
 #!/bin/bash
 
 
-
 filein=$1
 
-
-
 fileout=$2
-
-
 
 tmpfile="tmp.txt"
 
 
 
-
-
 if [[ ("$filein" == "-h") || ("$filein" == "") || ("$fileout" == "")]]; then
-
-
 
 	echo "Type domains filename first, and space separated output filename second";
 
-
-
 	exit 1;
-
-
 
 fi
 
 
-
-
-
-
-
 while IFS= read -r line
-
-	
 
 	do
 
-        	
-
         	assetfinder -subs-only "$line" | waybackurls | grep 'https' | grep 'PS9\|folder=\|download=\|file=\|dest=\|html=\|continue=\|=%3D\|=%2F\|%3D%2F\|return_to\|redirect_uri\|next=\|=/\|host=\|path=\|src=\|imgurl\|file=\|u=\|link=\|url=\|https%3A\|aHR0cHM6Ly' | grep -v 'utm_source\|utm_content\|metric\|signin\|signup\|login\|authorize\|click\|embed\|web.archive.org\|counter\|analytics\|utm_referrer';
-
-
-
-                                 
+                          
 
         done < "$filein" > "$tmpfile"
 
 
-
-
-
-
-
 cat "$tmpfile" | /root/test/urldedupe/./urldedupe -s > "$fileout"; #path to urldedupe tool
-
-
 
 rm "$tmpfile"
 
