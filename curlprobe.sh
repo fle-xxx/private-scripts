088daab2ef518a0b362ea014f3b5d6d323aa6fb0
za-sec <eremeevvanya1@gmail.com>
2021-05-05 17:59:56 +0300

    Update curlprobe.sh

--- curlprobe.sh
+++ curlprobe.sh
@@ -3,17 +3,23 @@
 #На вход даётся файл с доменами без протоколов. В каждой строчке один домен. На выходе - ответы по http/s
 
 filein=$1
+
 fileout=$2
+
 
 
         while IFS= read line
 
+
                 do 
 
-                        res1=$(curl -o /dev/null -s -w "%{http_code}\n" https://$line --connect-timeout 3)
-                        res2=$(curl -o /dev/null -s -w "%{http_code}\n" http://$line --connect-timeout 3)
+
+                        res1=$(curl -o /dev/null -s -w "%{http_code}\n" https://$line --connect-timeout 5)
+                        res2=$(curl -o /dev/null -s -w "%{http_code}\n" http://$line --connect-timeout 5)
+
 
                         echo -n $line; echo -n ','; echo -n $res1; echo -n ','; echo -n $res2; echo -en '\n'; 
 
 
-                done <"$filein" | grep -v '000,000' > "$fileout"
+                done <"$filein" | grep '200,\|403,\|404,\|401,\|405,\|204,\|206,\|400,\|406,\|410,\|500,\|501,\|502,\|503,\|,200\|,403\|,404\|,401\|,405\|,204\|,206\|,400\|,406\|,410\|,500\|,501\|,502\|,503' > "$fileout"
+
