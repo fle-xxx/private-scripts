dd35d6e30e33fc9109794749edc1df11279e4dc1
root <eremeevvanya1@gmail.com>
2021-06-05 16:54:34 +0300

    add cidr.sh

---
+++ cidr.sh
@@ -0,0 +1,16 @@
+filein=$1 #cidr scope file
+
+
+        while IFS= read line
+
+                do 
+
+                        nmap -sL -n $line | grep 'Nmap scan report for' | cut -f 5 -d ' ' >> cidr
+
+
+                done <"$filein"
+
+
+	wc cidr;
+
+
