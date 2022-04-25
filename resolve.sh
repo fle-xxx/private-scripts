1a91823942f909c70f2f6edb4a166a7a74c8e344
root <eremeevvanya1@gmail.com>
2022-04-25 14:13:51 +0300

    resolve

---
+++ resolve.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+filein=$1 # http/s:// url file
+
+filesub=filesub
+
+grep -r -h -Eo '[^/"]+' $filein | grep -v 'http' >> $filesub;
+
+#sort -u -o $filesub $filesub;
+
+
+        while IFS= read line
+
+                do 
+
+
+			
+
+                        addr=$(nslookup $line | grep 'Address' | grep -v '#' | sed 's/^.........//')
+
+                        #echo -n $line; echo -n ','; 
+			echo -n $addr; echo -en '\n'; 
+
+
+                done <"$filesub"
+
+rm filesub;
