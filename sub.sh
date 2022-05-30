d000b071c1eae4deca3809a2eb874f28e9d9dff8
fle-xxx <eremeevvanya1@gmail.com>
2022-05-30 18:41:12 +0300

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -3,25 +3,34 @@ filein=$1 #scope domains file
 
         while IFS= read line
 
-                do 
+         do 
 
-                        amass enum -active -brute -d $line >> subN;
+         	amass enum -active -brute -d $line >> subN;
 
 
-                done <"$filein"
+         done <"$filein"
 
 
 
         while IFS= read line
 
-                do 
+        do 
 
-                        assetfinder --subs-only $line >> subN;
+        	assetfinder --subs-only $line >> subN;
 
 
-                done <"$filein"
+        done <"$filein"
+
+
+	while IFS= read line
+
+        do
+
+		curl -fsSL -path-as-is "https://crt.sh/?CN=%25.""{$line}" | grep $line | sort -n | uniq -c | sort -r | column -t | grep -v '\*\|\#' | sed 's/^....//' | sed 's/[[:space:]]*$//' | grep '<TD>' | grep -oE '[[:alnum:]]+[.][[:alnum:]_.-]+' | grep $line >> subN;
 		
 		
+	done <"$filein"
+
 
         sort -u subN >> sub;
 
