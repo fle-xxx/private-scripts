990a6f73edbcf433988857269f2c9b0ffa0e1d26
fle_xxx <eremeevvanya1@gmail.com>
2022-07-15 06:42:14 -0400

    jscheck update

--- jscheck.sh
+++ jscheck.sh
@@ -12,6 +12,9 @@ cat jsout_$host | grep -v '\.js';
 cat jsout_$host | grep 'http.*\.js' | grep -v '\.json' >> jsurls_$host;
 
 
+cat jsurls_$host >> jsurls;
+ 
+
 cat jsurls_$host | xargs -I@ zsh -c "echo -e '\n[URL]: @\n'; GoLinkFinder -d @ | grep -v -e 'multipart\/\|UTC\|Pacific\|Australia\|Asia\|MM\|DD\|YY\|application\/\|Africa\|America\|GMT\|Europe\|text\/css\|image\/jpeg\|favicon\.ico\|^\.\|w3\.org\|^$\|\.png\|\.jpg\|googletagmanager\|google-analytics\|facebook\|instagram\|google\.com\|apple\.com\|\.css\|appgallery\|gtm\|woff\|image\/png\|text\/javascript\|\.svg\|speedcurve'"
 
 
